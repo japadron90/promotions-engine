b5a47a1ab315dfb5ef6d0fcec584956a088ca493
Julio <japadron90@gmail.com>
2024-02-03 18:27:45 -0500

    esto

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -7,13 +7,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Serializer;
 
 class ProductsController extends AbstractController
 {
     #[Route('/products/{id}/lowest-price', name: 'lowest-price',methods: 'POST')]
-    public function lowestPrice(int $id):Response{
-        dd($id);
-
+    public function lowestPrice(int $id){
+$something= new \DateTime();
+$serializer = new Serializer();
+dd("holaaaa");
+     /*   $contextBuilder = (new DateTimeNormalizerContextBuilder())->withFormat('Y-m-d H:i:s');
+        $serializer->serialize($something, 'json', $contextBuilder->toArray());
+        dd($serializer);
+*/
     }
 
 
