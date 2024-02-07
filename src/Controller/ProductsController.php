67a137e1f4227f4909c7cb18019dd1ac305d7969
Julio Antonio Padron Quintana <japadron90@gmail.com>
2024-02-06 23:35:26 -0500

    creando LowestPriceEnquiry.php

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Psr\Container\ContainerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
@@ -14,14 +15,31 @@ use Symfony\Component\Serializer\Context\Normalizer\DateTimeNormalizerContextBui
 
 class ProductsController extends AbstractController
 {
-    #[Route('/products/{id}/lowest-price', name: 'lowest-price',methods: 'GET')]
-    public function lowestPrice(int $id){
-        $encoders = [new JsonEncoder()];
+    #[Route('/products/{id}/lowest-price', name: 'lowest-price',methods: 'POST')]
+    public function lowestPrice(int $id,Request $request): Response{
+
+        if($request->headers->has('force_fail')){
+            return new JsonResponse(['error'=>'promotions Engine failure message'],$request->headers->get('force_fail'));
+
+        }
+        return new JsonResponse([
+            'quantity'=>5,
+            'request_location'=>'UK',
+            'voucher_code'=>'OU812',
+            'request_date'=>'2024-02-04',
+            'product_id'=>$id,
+            'price'=>100,
+            'discounted_price'=>50,
+            'promotion_id'=>3,
+            'promotion_name'=>'Black Friday half price sale'
+        ],200);
+        
+        /*$encoders = [new JsonEncoder()];
         $normalizers = [new ObjectNormalizer()];
 $tiempo= new \DateTime();
         $serializer = new Serializer($normalizers, $encoders);
 $json=$serializer->serialize($tiempo,'json');
-dd($json);
+dd($json);*/
     }
 
 
