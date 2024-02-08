6784fab61bb44ad4d6916b70352a1eaf5c408939
Julio Antonio Padron Quintana <japadron90@gmail.com>
2024-02-07 19:40:52 -0500

    Creando servicio

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\DTO\LowestPriceEnquiry;
+use App\Service\Serializer\DtoSerializer;
 use Psr\Container\ContainerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -18,7 +19,7 @@ use Symfony\Component\Serializer\SerializerInterface;
 class ProductsController extends AbstractController
 {
     #[Route('/products/{id}/lowest-price', name: 'lowest-price',methods: 'POST')]
-    public function lowestPrice(int $id,Request $request,SerializerInterface $serializer): Response{
+    public function lowestPrice(int $id,Request $request,DtoSerializer $serializer): Response{
 
         if($request->headers->has('force_fail')){
             return new JsonResponse(['error'=>'promotions Engine failure message'],$request->headers->get('force_fail'));
--- src/Service/Serializer/DtoSerializer.php
+++ src/Service/Serializer/DtoSerializer.php
@@ -2,22 +2,21 @@
 
 namespace App\Service\Serializer;
 
-use phpDocumentor\Reflection\DocBlock\Serializer;
+
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Serializer\SerializerInterface;
 
 class DtoSerializer implements SerializerInterface
 {
 private SerializerInterface $serializer ;
 
-    /**
-     * @param SerializerInterface $serializer
-     */
     public function __construct()
     {
-       $this->serializer = new Serializer([new ObjectNormalizer()],[ new JsonEncoder()]);
+        $this->serializer=new Serializer([new ObjectNormalizer()],[new JsonEncoder()]);
     }
+
 
     public function serialize(mixed $data, string $format, array $context = []): string
     {
