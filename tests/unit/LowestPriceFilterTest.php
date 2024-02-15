e6e3455e48904889c5ad0f8eb8553fbc8220a2a2
Julio Antonio Padron Quintana <japadron90@gmail.com>
2024-02-14 21:54:00 -0500

    return

---
+++ tests/unit/LowestPriceFilterTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Tests\unit;
+
+use App\Tests\ServiceTestCase;
+
+class LowestPriceFilterTest extends ServiceTestCase
+{
+    /**@test */
+public function lowest_price_promotions_filtering_is_aplied_correctly():void{
+
+    //given
+
+    //when
+
+    //then
+}
+}
