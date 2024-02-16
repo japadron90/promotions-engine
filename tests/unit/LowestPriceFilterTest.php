d91fcdb29b7ffac5f4de96456e4625168fa9dde3
Julio Antonio Padron Quintana <japadron90@gmail.com>
2024-02-16 18:47:12 -0500

    return

--- tests/unit/LowestPriceFilterTest.php
+++ tests/unit/LowestPriceFilterTest.php
@@ -4,8 +4,11 @@ namespace App\Tests\unit;
 
 
 
+use App\DTO\LowestPriceEnquiry;
+use App\Entity\Promotion;
 use App\Filter\LowestPriceFilter;
 use App\Tests\ServiceTestCase;
+use PhpParser\Node\Expr\Array_;
 
 class LowestPriceFilterTest extends ServiceTestCase
 {
@@ -13,13 +16,36 @@ class LowestPriceFilterTest extends ServiceTestCase
 public function testlowest_price_promotions_filtering_is_aplied_correctly():void{
 
     //given
+    $enquiry= new LowestPriceEnquiry();
+    $promotions=$this->promotionsDataProvider();
 $lowestPriceFilter=$this->container->get(LowestPriceFilter::class);
-   dd($lowestPriceFilter);
+
     //when
     $filteredEnquery=$lowestPriceFilter->apply($enquiry,...$promotions);
     //then
     $this->assertSame(100,$filteredEnquery->getPrice());
     $this->assertSame(50,$filteredEnquery->getDiscountedPrice());
     $this->assertSame('Black Friday half price sale',$filteredEnquery->getPromotionName());
+}
+public function promotionsDataProvider():array{
+    $promotionsOne= new Promotion();
+    $promotionsOne->setName('Black Friday half price sale');
+    $promotionsOne->setAdjustment(0.5);
+    $promotionsOne->setCriteria(["from"=>"2022-11-25","to"=>"2022-11-28"]);
+    $promotionsOne->setType('date_range_multiplaier');
+
+    $promotionsTwo= new Promotion();
+    $promotionsTwo->setName('Voucher OU812');
+    $promotionsTwo->setAdjustment(100);
+    $promotionsTwo->setCriteria(["code"=>"OU812"]);
+    $promotionsTwo->setType('fixed_price_voucher');
+
+    $promotionsThree= new Promotion();
+    $promotionsThree->setName('Buy one get one free');
+    $promotionsThree->setAdjustment(0.5);
+    $promotionsThree->setCriteria(["minimum_quantity"=>2]);
+    $promotionsThree->setType('date_range_multiplaier');
+
+   return [$promotionsOne,$promotionsTwo,$promotionsThree];
 }
 }
