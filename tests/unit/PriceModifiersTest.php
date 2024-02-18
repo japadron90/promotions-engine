df31eef9404a5226471d5b0945e490b411e62838
Julio Antonio Padron Quintana <japadron90@gmail.com>
2024-02-18 11:09:51 -0500

    return

--- tests/unit/PriceModifiersTest.php
+++ tests/unit/PriceModifiersTest.php
@@ -10,32 +10,49 @@ use App\Tests\ServiceTestCase;
 
 class PriceModifiersTest extends ServiceTestCase
 {
-/**  @test */
-
-public function DateRangeMultiplier_returns_a_correctly_modified_price():void{
-//given
-$enquiry=new LowestPriceEnquiry();
-$enquiry->setQuantity(5);
-$enquiry->setRequestDate('2022-11-27');
-
-  $promotion= new Promotion();
-    $promotion->setName('Black Friday half price sale');
-    $promotion->setAdjustment(0.5);
-    $promotion->setCriteria(["from"=>"2022-11-25","to"=>"2022-11-28"]);
-    $promotion->setType('date_range_multiplaier');
-
-
-    $dateRangeModified=new DateRangeMultiplier();
-  //when
-    $modifiedPrice=$dateRangeModified->modify(100,5,$promotion,$enquiry);
-
-    //then
-    $this->assertEquals(250,$modifiedPrice);
-}
     /**  @test */
-public function FixedPriceVoucher_returns_acorrectly_modified_price():void{
-    $fixedPriceVoucher= new FixedPriceVoucher();
+
+    public function DateRangeMultiplier_returns_a_correctly_modified_price(): void
+    {
+//given
+        $enquiry = new LowestPriceEnquiry();
+        $enquiry->setQuantity(5);
+        $enquiry->setRequestDate('2022-11-27');
+
+        $promotion = new Promotion();
+        $promotion->setName('Black Friday half price sale');
+        $promotion->setAdjustment(0.5);
+        $promotion->setCriteria(["from" => "2022-11-25", "to" => "2022-11-28"]);
+        $promotion->setType('date_range_multiplaier');
 
 
-}
+        $dateRangeModified = new DateRangeMultiplier();
+        //when
+        $modifiedPrice = $dateRangeModified->modify(100, 5, $promotion, $enquiry);
+
+        //then
+        $this->assertEquals(250, $modifiedPrice);
+    }
+
+    /**  @test */
+    public function FixedPriceVoucher_returns_a_correctly_modified_price(): void
+    {
+        $fixedPriceVoucher = new FixedPriceVoucher();
+
+        $promotion = new Promotion();
+        $promotion->setName('Voucher OU812');
+        $promotion->setAdjustment(100);
+        $promotion->setCriteria(["code" => "OU812"]);
+        $promotion->setType('fixed_price_voucher');
+
+        $enquiry = new LowestPriceEnquiry();
+        $enquiry->setQuantity(5);
+        $enquiry->setVoucherCode('OU812');
+
+        $modifiedPrice=$fixedPriceVoucher->modify(150,5,$promotion,$enquiry);
+
+        $this->assertEquals(500,$modifiedPrice);
+
+
+    }
 }
