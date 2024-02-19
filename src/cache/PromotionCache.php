f78ff9e0ee703882c167eb7854702f85021ad422
Julio Antonio Padron Quintana <japadron90@gmail.com>
2024-02-19 15:45:07 -0500

    eliminando entity manager

--- src/cache/PromotionCache.php
+++ src/cache/PromotionCache.php
@@ -20,10 +20,11 @@ private $repository;
         $this->cache=$cache;
         $this->repository=$repository;
     }
-    public function findValidForProduct(Product $product,string  $requestDate){
+    public function findValidForProduct(Product $product,string  $requestDate):?array{
         $key=sprintf("find-valid-for-product-%d",$product->getId());
      return $this->cache->get($key,function (ItemInterface $item) use ($product,$requestDate){
            var_dump('miss');
+           $item->expiresAfter(3600);
           return $this->repository->findValidForProduct($product,date_create_immutable($requestDate));
 
 
