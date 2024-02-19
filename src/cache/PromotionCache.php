89915dfbe2585f1241da9c312c036ee605bfd88e
Julio Antonio Padron Quintana <japadron90@gmail.com>
2024-02-19 15:34:19 -0500

    caching

---
+++ src/cache/PromotionCache.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\cache;
+
+
+
+use App\DTO\LowestPriceEnquiry;
+use App\DTO\PriceEnquiryInterface;
+use App\Entity\Product;
+use App\Repository\PromotionRepository;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
+
+class PromotionCache
+{
+private $cache;
+private $repository;
+    public function __construct(CacheInterface $cache,PromotionRepository $repository)
+    {
+        $this->cache=$cache;
+        $this->repository=$repository;
+    }
+    public function findValidForProduct(Product $product,string  $requestDate){
+        $key=sprintf("find-valid-for-product-%d",$product->getId());
+     return $this->cache->get($key,function (ItemInterface $item) use ($product,$requestDate){
+           var_dump('miss');
+          return $this->repository->findValidForProduct($product,date_create_immutable($requestDate));
+
+
+       });
+    }
+}
